9d0584f84b0ece9915f8cdd88ff2ec4ae68af5da
JackyC415 <32407740+JackyC415@users.noreply.github.com>
2020-03-28 13:44:20 -0700

    add test schema

--- aws_service/models/Test.js
+++ aws_service/models/Test.js
@@ -4,3 +4,32 @@
  Upload Test Suite
  Submit Test
  */
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const testSchema = new Schema({
+    awsRunId: String,
+    _test: '',
+    type: {
+        type: String,
+        enum: ['Physical', 'Virtual'],
+        default: "Virtual"
+    },
+    created: { type: Date, default: Date.now },
+    status: {
+        type: String,
+        emum: ['pending', 'processing', 'scheduling', 'preparing', 'running',
+        'completed', 'stopping'],
+        default: 'none'
+    },
+    result: {
+        type: String,
+        emum: ['pending', 'passed', 'warned', 'failed', 'skipped',
+        'errored', 'stopped'],
+        default: 'none'
+    },
+    started: { type: Date, default: Date.now},
+    stopped: Date
+});
+
+mongoose.model('Test', testSchema);
