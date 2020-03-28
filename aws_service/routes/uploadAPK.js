4db6e9b62981aa80bf0d853757b5723dca5d5850
JackyC415 <32407740+JackyC415@users.noreply.github.com>
2020-03-28 13:33:52 -0700

    getUpload, scheduleRun

--- aws_service/routes/uploadAPK.js
+++ aws_service/routes/uploadAPK.js
@@ -10,13 +10,15 @@ module.exports = function upload(req, res) {
     });
 
     var params = {
-        arn: 'STRING_VALUE' 
+        arn: 'STRING_VALUE'
     };
     devicefarm.getUpload(params, function (err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else console.log(data);           // successful response
     });
+    */
 
+    /*
     var params = {
         name: req.body.name,
         type: "APPIUM_PYTHON_TEST_PACKAGE",
@@ -27,7 +29,9 @@ module.exports = function upload(req, res) {
         if (err) console.log(err, err.stack); // an error occurred
         else console.log(data);           // successful response
 
-    });*/
+    });
+    */
+
 
     /*
     var form = new IncomingForm();
@@ -40,4 +44,22 @@ module.exports = function upload(req, res) {
         res.json();
     });
     form.parse(req);*/
+}
+
+module.exports = function scheduleRun(req, res) {
+
+    /*
+    var params = {
+        name: "MyRun",
+        devicePoolArn: "arn:aws:devicefarm:us-west-2:123456789101:pool:EXAMPLE-GUID-123-456", // You can get the Amazon Resource Name (ARN) of the device pool by using the list-pools CLI command.
+        projectArn: "arn:aws:devicefarm:us-west-2:123456789101:project:EXAMPLE-GUID-123-456", // You can get the Amazon Resource Name (ARN) of the project by using the list-projects CLI command.
+        test: {
+            type: "APPIUM_JAVA_JUNIT",
+            testPackageArn: "arn:aws:devicefarm:us-west-2:123456789101:test:EXAMPLE-GUID-123-456"
+        }
+    };
+    devicefarm.scheduleRun(params, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data);           // successful response
+    }*/
 }
