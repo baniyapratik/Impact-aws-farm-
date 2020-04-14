047b688096261e30ef5721b7f38a4794adbb0260
JackyC415 <32407740+JackyC415@users.noreply.github.com>
2020-04-14 15:24:11 -0700

    integrate react-charts to test run result

--- client/src/components/TestRunner/GetRun.js
+++ client/src/components/TestRunner/GetRun.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { Pie } from "react-chartjs-2";
+import { MDBContainer } from "mdbreact";
 
 class GetRun extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            run: []
+            run: [],
+            dataPie: {
+                labels: ["FAILED", "PASSED", "SKIPPED", "WARNED", "STOPPED"],
+                datasets: [
+                    {
+                        data: [],
+                        backgroundColor: [
+                            "#F7464A",
+                            "#46BFBD",
+                            "#FDB45C",
+                            "#949FB1",
+                            "#4D5360",
+                            "#AC64AD"
+                        ],
+                        hoverBackgroundColor: [
+                            "#FF5A5E",
+                            "#5AD3D1",
+                            "#FFC870",
+                            "#A8B3C5",
+                            "#616774",
+                            "#DA92DB"
+                        ]
+                    }
+                ]
+            }
         }
     }
 
@@ -14,7 +40,6 @@ class GetRun extends Component {
         let arn = this.props.match.params.id + "/" + this.props.match.params.id2;
         axios.get('/aws-testrunner/getrun/' + arn)
             .then(res => {
-                console.log(res.data);
                 this.setState({ run: res.data });
             }).catch((err) => {
                 console.log(err);
@@ -22,23 +47,34 @@ class GetRun extends Component {
     }
 
     renderRuns() {
+        let totalMins = null;
         return Object.keys(this.state.run).map((i, index) => {
+            if (this.state.run[i].deviceMinutes) {
+                totalMins = <td>{this.state.run[i].deviceMinutes.total}</td>
+            }
+            { this.state.dataPie.datasets[0].data[0] = this.state.run[i].counters.failed }
+            { this.state.dataPie.datasets[0].data[1] = this.state.run[i].counters.passed }
+            { this.state.dataPie.datasets[0].data[2] = this.state.run[i].counters.skipped }
+            { this.state.dataPie.datasets[0].data[3] = this.state.run[i].counters.warned }
+            { this.state.dataPie.datasets[0].data[4] = this.state.run[i].counters.stopped }
             return (
                 <div key={index}>
                     <table id="runs">
                         <tr>
                             <th>Run</th>
-                            <th>STOPPED</th>
-                            <th>SKIPPED</th>
-                            <th>PASSED</th>
+                            <th>Type</th>
+                            <th>Platform</th>
+                            <th>Status</th>
+                            <th>Result</th>
                             <th>Total minutes</th>
                         </tr>
                         <tr>
                             <td>{this.state.run[i].name}</td>
-                            <td>{this.state.run[i].counters.stopped}</td>
-                            <td>{this.state.run[i].counters.skipped}</td>
-                            <td>{this.state.run[i].counters.passed}</td>
-                            <td>{this.state.run[i].deviceMinutes.total}</td>
+                            <td>{this.state.run[i].type}</td>
+                            <td>{this.state.run[i].platform}</td>
+                            <td>{this.state.run[i].status}</td>
+                            <td>{this.state.run[i].result}</td>
+                            {totalMins}
                         </tr>
                     </table>
                 </div>
@@ -50,6 +86,9 @@ class GetRun extends Component {
         return (
             <div>
                 {this.renderRuns()}
+                <MDBContainer>
+                    <Pie data={this.state.dataPie} options={{ responsive: true }} />
+                </MDBContainer>
             </div>
         )
     }
