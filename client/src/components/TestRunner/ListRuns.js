0ca335bfea1c12e4a4eb7413febd7630d919964f
Rajeev Sebastian <rajeev.sebastian@sjsu.edu>
2020-05-11 14:34:34 -0700

    List Runs Table Partial

--- client/src/components/TestRunner/ListRuns.js
+++ client/src/components/TestRunner/ListRuns.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import './testrunner.css';
-import * as routes from './../authentication/constants/routes';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "./testrunner.css";
+import * as routes from "./../authentication/constants/routes";
+import { Table, Button, Alert } from "react-bootstrap";
 
 class ListRuns extends Component {
   constructor(props) {
@@ -18,7 +19,7 @@ class ListRuns extends Component {
     axios
       .get(`/api/aws-testrunner/listruns/${this.state.project.projectarn}`)
       .then((res) => {
-        if (res.data.runs[0].status !== 'COMPLETED') {
+        if (res.data.runs[0].status !== "COMPLETED") {
           this.state.pendingStatus = true;
         } else {
           this.state.pendingStatus = false;
@@ -46,6 +47,40 @@ class ListRuns extends Component {
     return Object.keys(this.state.runs).map((i, index) => {
       return (
         <div key={index}>
+          <Table
+            striped
+            bordered
+            hover
+            responsive
+            variant="dark"
+            style={{ marginTop: "15px", textAlign: "center" }}
+          >
+            <thead>
+              <tr>
+                <th>Run Details</th>
+                <th>Status</th>
+                <th>Result</th>
+                <th>View Run</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.runs[i].map((run, index2) => (
+                <tr key={index2} data-value={run.arn}>
+                  <td>
+                    Name : {run.name} {"\n"}
+                    Type : {run.type} {"\n"}
+                    Platform : {run.platform} {"\n"}
+                    Created on : {run.created} {"\n"}
+                  </td>
+                  <td>{run.status} </td>
+                  <td>{run.result} </td>
+                  <td>
+                    <Link to={"/run/" + run.arn}>{buttonRender}</Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
           <table id="runs">
             <tr>
               <th>Run</th>
@@ -65,7 +100,7 @@ class ListRuns extends Component {
                 <td>{run.result} </td>
                 <td>{run.created} </td>
                 <td>
-                  <Link to={'/run/' + run.arn}>{buttonRender}</Link>
+                  <Link to={"/run/" + run.arn}>{buttonRender}</Link>
                 </td>
               </tr>
             ))}
