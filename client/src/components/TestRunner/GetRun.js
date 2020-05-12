ccd6d4f29c4872ce8f83129531aad8ebdad8c7d7
Rajeev Sebastian <rajeev.sebastian@sjsu.edu>
2020-05-11 19:40:22 -0700

    get run status charts

--- client/src/components/TestRunner/GetRun.js
+++ client/src/components/TestRunner/GetRun.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import * as routes from './../authentication/constants/routes';
 import CanvasJSReact from '../../assets/canvasjs.react';
+import { Table, Button, Alert } from 'react-bootstrap';
+
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 class GetRun extends Component {
@@ -10,16 +12,40 @@ class GetRun extends Component {
     this.state = {
       run: [],
       pieChart: {},
+      arn: this.props.arn,
     };
   }
 
   componentDidMount() {
-    let arn = this.props.match.params.id + '/' + this.props.match.params.id2;
+    //console.log('First Part ----' + this.props.match.params.id);
+    // console.log('Second Part ----' + this.props.match.params.id2);
+    //let arn = this.props.match.params.id + '/' + this.props.match.params.id2;
+    let arn = this.state.arn;
     axios
       .get('/api/aws-testrunner/getrun/' + arn)
       .then((res) => {
-        this.setState({ run: res.data });
-        this.setState({ pieChart: { dataPoints: [{ y: 5, label: 'b' }] } });
+        this.setState({
+          pieChart: { dataPoints: [{ y: 5, label: 'b' }] },
+          run: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  componentDidUpdate() {
+    //console.log('First Part ----' + this.props.match.params.id);
+    // console.log('Second Part ----' + this.props.match.params.id2);
+    //let arn = this.props.match.params.id + '/' + this.props.match.params.id2;
+    let arn = this.state.arn;
+    axios
+      .get('/api/aws-testrunner/getrun/' + arn)
+      .then((res) => {
+        this.setState({
+          pieChart: { dataPoints: [{ y: 5, label: 'b' }] },
+          run: res.data,
+        });
       })
       .catch((err) => {
         console.log(err);
@@ -93,24 +119,35 @@ class GetRun extends Component {
 
       return (
         <div key={index}>
-          <table id="runs">
-            <tr>
-              <th>Run</th>
-              <th>Type</th>
-              <th>Platform</th>
-              <th>Status</th>
-              <th>Result</th>
-              <th>Total minutes</th>
-            </tr>
-            <tr>
-              <td>{this.state.run[i].name}</td>
-              <td>{this.state.run[i].type}</td>
-              <td>{this.state.run[i].platform}</td>
-              <td>{this.state.run[i].status}</td>
-              <td>{this.state.run[i].result}</td>
-              {totalMins}
-            </tr>
-          </table>
+          <Table
+            striped
+            bordered
+            hover
+            responsive
+            variant="dark"
+            style={{ marginTop: '15px', textAlign: 'center' }}
+          >
+            <thead>
+              <tr>
+                <th>Run</th>
+                <th>Type</th>
+                <th>Platform</th>
+                <th>Status</th>
+                <th>Result</th>
+                <th>Total minutes</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{this.state.run[i].name}</td>
+                <td>{this.state.run[i].type}</td>
+                <td>{this.state.run[i].platform}</td>
+                <td>{this.state.run[i].status}</td>
+                <td>{this.state.run[i].result}</td>
+                {totalMins}
+              </tr>
+            </tbody>
+          </Table>
         </div>
       );
     });
--- client/src/components/TestRunner/ListRuns.js
+++ client/src/components/TestRunner/ListRuns.js
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import "./testrunner.css";
-import * as routes from "./../authentication/constants/routes";
-import { Table, Button, Alert } from "react-bootstrap";
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import './testrunner.css';
+import * as routes from './../authentication/constants/routes';
+import { Table, Button, Alert } from 'react-bootstrap';
+import GetRun from './GetRun';
 
 class ListRuns extends Component {
   constructor(props) {
@@ -12,6 +13,8 @@ class ListRuns extends Component {
       runs: [],
       pendingStatus: false,
       project: this.props,
+      showChart: false,
+      currenttestarn: null,
     };
   }
 
@@ -19,27 +22,74 @@ class ListRuns extends Component {
     axios
       .get(`/api/aws-testrunner/listruns/${this.state.project.projectarn}`)
       .then((res) => {
-        if (res.data.runs[0].status !== "COMPLETED") {
-          this.state.pendingStatus = true;
+        let status = null;
+        if (res.data.runs[0].status !== 'COMPLETED') {
+          status = true;
         } else {
-          this.state.pendingStatus = false;
+          status = false;
         }
-        this.setState({ runs: res.data });
+        this.setState({ runs: res.data, pendingStatus: status });
       })
       .catch((err) => {
         console.log(err);
       });
   }
 
+  showPieChart = (arn, event) => {
+    this.setState({ showChart: true, currenttestarn: arn });
+  };
+
+  getPieChart() {
+    const currenttestarn = this.state.currenttestarn;
+    return (
+      <div>
+        <h4>Completed Status</h4>
+        <GetRun {...{ arn: currenttestarn }}></GetRun>
+      </div>
+    );
+  }
+
+  buttonRender = (arn) => {
+    const pendingStatus = this.state.pendingStatus;
+    if (!pendingStatus) {
+      return (
+        <Button
+          variant="light"
+          onClick={(event) => this.showPieChart(arn, event)}
+        >
+          View
+        </Button>
+      );
+    }
+
+    return (
+      <>
+        <Button
+          variant="light"
+          onClick={(event) => this.showPieChart(arn, event)}
+          disabled
+        >
+          View
+        </Button>
+      </>
+    );
+  };
+
   renderRuns() {
+    const showChart = this.state.showChart;
     let buttonRender = (
-      <button type="button" class="btn btn-lg btn-primary" enabled>
+      <button
+        type="button"
+        class="btn btn-lg btn-primary"
+        disabled={false}
+        onClick={this.showPieChart}
+      >
         View
       </button>
     );
     if (this.state.pendingStatus) {
       buttonRender = (
-        <button type="button" class="btn btn-lg btn-primary" disabled>
+        <button type="button" class="btn btn-lg btn-primary" disabled={true}>
           View
         </button>
       );
@@ -47,64 +97,43 @@ class ListRuns extends Component {
     return Object.keys(this.state.runs).map((i, index) => {
       return (
         <div key={index}>
-          <Table
-            striped
-            bordered
-            hover
-            responsive
-            variant="dark"
-            style={{ marginTop: "15px", textAlign: "center" }}
-          >
-            <thead>
-              <tr>
-                <th>Run Details</th>
-                <th>Status</th>
-                <th>Result</th>
-                <th>View Run</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.runs[i].map((run, index2) => (
-                <tr key={index2} data-value={run.arn}>
-                  <td>
-                    Name : {run.name} {"\n"}
-                    Type : {run.type} {"\n"}
-                    Platform : {run.platform} {"\n"}
-                    Created on : {run.created} {"\n"}
-                  </td>
-                  <td>{run.status} </td>
-                  <td>{run.result} </td>
-                  <td>
-                    <Link to={"/run/" + run.arn}>{buttonRender}</Link>
-                  </td>
+          <center>
+            <hr></hr>
+            <h4>Test Status</h4>
+            <Table
+              striped
+              bordered
+              hover
+              responsive
+              variant="dark"
+              style={{ marginTop: '15px', textAlign: 'center' }}
+            >
+              <thead>
+                <tr>
+                  <th>Run Details</th>
+                  <th>Status</th>
+                  <th>Result</th>
+                  <th>View Run</th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
-          <table id="runs">
-            <tr>
-              <th>Run</th>
-              <th>Type</th>
-              <th>Platform</th>
-              <th>Status</th>
-              <th>Result</th>
-              <th>Created</th>
-              <th>View Run</th>
-            </tr>
-            {this.state.runs[i].map((run, index2) => (
-              <tr key={index2} data-value={run.arn}>
-                <td>{run.name} </td>
-                <td>{run.type} </td>
-                <td>{run.platform} </td>
-                <td>{run.status} </td>
-                <td>{run.result} </td>
-                <td>{run.created} </td>
-                <td>
-                  <Link to={"/run/" + run.arn}>{buttonRender}</Link>
-                </td>
-              </tr>
-            ))}
-          </table>
+              </thead>
+              <tbody>
+                {this.state.runs[i].map((run, index2) => (
+                  <tr key={index2} data-value={run.arn}>
+                    <td>
+                      Name : {run.name} <br />
+                      Type : {run.type} <br />
+                      Platform : {run.platform} <br />
+                      Created on : {run.created} <br />
+                    </td>
+                    <td>{run.status} </td>
+                    <td>{run.result} </td>
+                    <td>{this.buttonRender(run.arn)}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+            <div>{showChart && this.getPieChart()}</div>
+          </center>
         </div>
       );
     });
